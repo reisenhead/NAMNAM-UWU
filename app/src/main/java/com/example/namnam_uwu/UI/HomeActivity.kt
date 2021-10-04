fcfaeef460688b931f64ce4ce3e9f17e8aef8724
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-10-03 23:47:16 -0500

    Agregando Notificación Simple

--- app/src/main/java/com/example/namnam_uwu/UI/HomeActivity.kt
+++ app/src/main/java/com/example/namnam_uwu/UI/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.namnam_uwu.UI
 
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
+import androidx.core.app.NotificationCompat
 import com.example.namnam_uwu.Fragments.CardsFragment
 import com.example.namnam_uwu.Controller.RegisterProduct
 import com.example.namnam_uwu.R
@@ -20,8 +28,6 @@ enum class ProvidertType{
 var email = String()
 var provider = String()
 
-const val USER_NAME = "com.example.namnam_uwu.UI.USER_NAME"
-
 var num = 0
 var positionE = 0
 var titles = mutableListOf("Amamba", "Cachito Mío", "Forever", "Forte", "Green Grass", "La Docena", "La Pitahaya Vegana", "Los Loosers", "Ramen Bar", "Soul La Roma")
@@ -32,8 +38,15 @@ var products = RegisterProduct()
 
 class HomeActivity : AppCompatActivity() {
 
+
+    companion object{
+        //el nombre de la acción a ejecutar por el botón en la notificación
+        const val CHANNEL_ID = "Ejemplo"
+    }
+
     private lateinit var boton: Button
     private lateinit var botontwo: Button
+    private lateinit var btnNotify : Button
     private lateinit var input: EditText
     private val cardFragment = CardsFragment()
     private val profileFragment = ProfileFragment()
@@ -47,6 +60,7 @@ class HomeActivity : AppCompatActivity() {
         replaceFragment(homeFragment)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //Recuperando los datos del usuario
@@ -58,9 +72,20 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         replaceFragment(homeFragment)
 
+
+        //Para android Oreo en adelante, s obligatorio registrar el canal de notificación
+        //Para android Oreo en adelante, s obligatorio registrar el canal de notificación
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setNotificationChannel()
+        }
+//        btnNotify = findViewById(R.id.btnNotify)
+//        btnNotify.setOnClickListener{
+//            simpleNotification()
+//        }
+
         botontwo = findViewById(R.id.buttonlist)
         botontwo.setOnClickListener {
-
+            simpleNotification()
             replaceFragment(cardFragment)
         }
         boton = findViewById(R.id.buttonworld)
@@ -89,6 +114,36 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
+
+
+    private fun simpleNotification(){
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.triforce) //seteamos el ícono de la push notification
+           //setColor(getColor(R.color.accent1)) //definimos el color del ícono y el título de la notificación
+            .setContentTitle(getString(R.string.simple_title)) //seteamos el título de la notificación
+            .setContentText(getString(R.string.simple_body)) //seteamos el cuerpo de la notificación
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT) //Ponemos una prioridad por defecto
+
+        //lanzamos la notificación
+        with(NotificationManagerCompat.from(this)) {
+            notify(20, builder.build()) //en este caso pusimos un id genérico
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setNotificationChannel(){
+        val name = getString(R.string.channel_courses)
+        val descriptionText = getString(R.string.courses_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+    }
 
 
     private fun replaceFragment(fragment: Fragment){
